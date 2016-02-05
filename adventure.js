ccfc29cb884ac3279ef6e9addf15b3d85ce7de3a
Andrew Maul <amaul@ops-frontend-2.local>
2016-02-04 19:45:15 -0700

    Gets take function working. Objects can now move zones from room to inventory and descriptions are accurately reflected.

--- adventure.js
+++ adventure.js
@@ -4,34 +4,50 @@ $(document).ready(function() {
 	var backpack = [];
 
 	// Things
-	function Item(itemName, itemDesc) {
+	function Item(itemName, itemDesc, itemInRoomDesc) {
 		this.itemName = itemName;
 		this.itemDesc = itemDesc;
+		this.itemInRoomDesc = itemInRoomDesc;
 	}
 	var pebble = new Item(
 		"pebble",
-		"A small, smooth pebble."
+		"A small, smooth pebble.",
+		"On the floor there is a pebble."
+		);
+	var torch = new Item(
+		"torch",
+		"A wooden haft with oil soaked rags wrapped around the top. It would readily burn.",
+		"A rusted iron bracket holds a single torch."
 		);
 	//Rooms
 	function Room(shortDesc, lDesc, things) {
 		this.shortDesc = shortDesc;
 		this.lDesc = lDesc;
+		this.lDescModified = lDesc;
 		this.things = things;
 	}
 
 	var cave = new Room(
 		"a dark, dank cave.", 
-		"This cave is drafty, and the walls slimy. On the floor there is a pebble.",
-		[pebble, "torch"]
+		"This cave is drafty, and the walls slimy.",
+		[pebble, torch]
 		);
-
+	
 	var currentRoom = cave;
+	
+	// Room Update Function
+	function updateRoomDesc() {
+		currentRoom.lDescModified =  currentRoom.lDesc;
+		for (var i = 0; i < currentRoom.things.length; i++) {
+			currentRoom.lDescModified = currentRoom.lDescModified + " " + currentRoom.things[i].itemInRoomDesc;
+		};
+	}
+	updateRoomDesc();
 
 	$('.message').text("You are in " + currentRoom.shortDesc);
 
 	// Array of Commands
 	var commandArray = ["look", "move", "take", "inventory"];
-
 
 	//Input Controls
 	$(document).keypress(function(e){
@@ -57,35 +73,36 @@ $(document).ready(function() {
 	// Look Command function
 	window.look = function(optionalObject) {
 		if(optionalObject) {
-			for (var i = currentRoom.things.length - 1; i >= 0; i--) {
+			var isAThingCounter = false;
+			for (var i = 0; i < currentRoom.things.length; i++) {
 				if (currentRoom.things[i].itemName == optionalObject) {
 					$('.message').text(currentRoom.things[i].itemDesc);
-				}
-				else {
-					$('.message').text("Not much to see here.");
+					isAThingCounter = true;
 				}
 			};
-			for (var i = backpack.length - 1; i >= 0; i--) {
+			for (var i = 0; i < backpack.length; i++) {
 				if (backpack[i].itemName == optionalObject) {
 					$('.message').text(backpack[i].itemDesc + " It is in your backpack.");
-				}
-				else {
-					$('.message').text("Not much to see here.");
+					isAThingCounter = true;
 				}
 			};
+			if (isAThingCounter == false) {
+				$('.message').text("Not much to see here.");
+			}
 		}
 		else {
-			$('.message').text(currentRoom.lDesc);
+			$('.message').text(currentRoom.lDescModified);
 		}
 	}
 	// Take Command function
 	window.take = function(optionalObject) {
 		if(optionalObject) {
-			for (var i = currentRoom.things.length - 1; i >= 0; i--) {
+			for (var i = 0; i < currentRoom.things.length; i++) {
 				if (currentRoom.things[i].itemName == optionalObject) {
 					$('.message').text("You take the " + currentRoom.things[i].itemName + ".");
 					backpack.push(currentRoom.things[i]);
 					currentRoom.things.splice(i,1);
+					updateRoomDesc();
 				}
 				else {
 					$('.message').text("You can't take that.");
@@ -100,7 +117,7 @@ $(document).ready(function() {
 	window.inventory = function() {
 		if(backpack.length > 0) {
 			var tempBackpack = [];
-			for (var i = backpack.length - 1; i >= 0; i--) {
+			for (var i = 0; i < backpack.length; i++) {
 				tempBackpack.push(backpack[i].itemName);
 			};
 			$('.message').text("You have " + tempBackpack.join(', '));
