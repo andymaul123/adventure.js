195e5c040faf3c0988b6b15d4eba4d8cd0b41cf3
andymaul123Zenbook <andymaul123@gmail.com>
2016-02-07 14:31:43 -0700

    Adds door and key objects to hall, and some basic fuctionality.

--- adventure.js
+++ adventure.js
@@ -43,15 +43,18 @@ Global Player Variables
 Items Galore!
 ================================================================================
 */
-	function Item(itemName, itemDesc, itemInRoomDesc) {
+	function Item(itemName, itemDesc, itemInRoomDesc, canBeTaken) {
 		this.itemName = itemName;
 		this.itemDesc = itemDesc;
 		this.itemInRoomDesc = itemInRoomDesc;
+		this.canBeTaken = canBeTaken;
 	}
+	// Room 1 Cave Items
 	var pebble = new Item(
 		"pebble",
 		"A small, smooth pebble.",
-		"On the floor there is a pebble."
+		"On the floor there is a pebble.",
+		true
 		);
 	pebble.activate = function() {
 		$('.message').text("It's a pebble. What would you expect to happen?");
@@ -59,11 +62,48 @@ Items Galore!
 	var torch = new Item(
 		"torch",
 		"A wooden haft with oil soaked rags wrapped around the top. It would readily burn.",
-		"A rusted iron bracket holds a single torch."
+		"A rusted iron bracket holds a single torch.",
+		true
 		);
 	torch.activate = function() {
 		playerHasLight = true;
 		$('.message').text("With a few sparks from your flint and steel the torch catches and roars to life.");
+	}
+	//Room 2 Hall Items
+	var key = new Item(
+		"key",
+		"Small iron key.",
+		"A small key twinkles in the flickering light of your torch.",
+		true
+		);
+	key.activate = function() {
+		if (currentRoom == hall){
+			doubleDoors.locked = false;
+			$('.message').text("With a twist the doors' lock disengages.");
+		}
+		else {
+			$('.message').text("Now is not the time to use that.");
+		}
+	}
+	var doubleDoors = new Item(
+		"doors",
+		"A set of double doors.",
+		"Massive double doors made from wood and reinforced with iron. They are locked.",
+		false
+		);
+	doubleDoors.locked = true;
+	doubleDoors.open = false;
+	doubleDoors.activate = function() {
+		if (doubleDoors.locked) {
+			$('.message').text("Try as you might, the doors won't budge.");
+		}
+		else if (doubleDoors.open == false) {
+			doubleDoors.open = true;
+			$('.message').text("With some effort you slowly push the double doors open.");
+		}
+		else {
+			$('.message').text("There's not much more to be done with the doors.");
+		}
 	}
 /*
 Rooms n' More
@@ -91,7 +131,7 @@ Rooms n' More
 		"hall",
 		"a long stone tunnel.",
 		"Hewn stone walls extend north creating a claustrophobic hall that culminates in a set of double-doors.",
-		["placeholder"],
+		[doubleDoors, key],
 		7,
 		true
 		);
@@ -150,10 +190,15 @@ Command Functions
 		if(optionalObject) {
 			for (var i = 0; i < currentRoom.things.length; i++) {
 				if (currentRoom.things[i].itemName == optionalObject) {
-					$('.message').text("You take the " + currentRoom.things[i].itemName + ".");
-					backpack.push(currentRoom.things[i]);
-					currentRoom.things.splice(i,1);
-					updateRoomDesc();
+					if(currentRoom.things[i].canBeTaken) {
+						$('.message').text("You take the " + currentRoom.things[i].itemName + ".");
+						backpack.push(currentRoom.things[i]);
+						currentRoom.things.splice(i,1);
+						updateRoomDesc();
+					}
+					else {
+						$('.message').text("You can't take that.");
+					}
 				}
 				else {
 					$('.message').text("You can't take that.");
@@ -212,12 +257,14 @@ Command Functions
 						isThereARoom++;
 						if (currentRoom.pitchBlack && playerHasLight) {
 							$('.message').text("You are in " + currentRoom.shortDesc);
+							updateRoomDesc();
 						}
 						else if (currentRoom.pitchBlack && playerHasLight == false) {
 							$('.message').text("The room is pitch black.");
 						}
 						else {
 							$('.message').text("You are in " + currentRoom.shortDesc);
+							updateRoomDesc();
 						}
 					}
 				};
@@ -240,6 +287,13 @@ Command Functions
 					canUse = true;
 				}
 			};
+			for (var i = 0; i < currentRoom.things.length; i++) {
+				if (currentRoom.things[i].itemName == requiredObject) {
+					currentRoom.things[i].activate();
+					canUse = true;
+				}
+			};
+
 			if (canUse == false) {
 				$('.message').text("You don't have a " + requiredObject + " to use.");
 			}
